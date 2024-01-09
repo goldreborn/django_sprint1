5504071709d2d64a126eed7c8ce195945dd8bdda
Яков Кузнецов <yakov.kuznetsov98@yandex.ru>
2024-01-09 21:36:12 +0300

    update

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 
-from django.views.generic.base import TemplateView as tv
+from django.views.generic.base import TemplateView as TV
 
 app_name = 'pages'
 
 urlpatterns = [
-    path('about/', tv.as_view(template_name="pages/about.html"), name='about'),
-    path('rules/', tv.as_view(template_name="pages/rules.html"), name='rules'),
+    path('about/', TV.as_view(template_name="pages/about.html"), name='about'),
+    path('rules/', TV.as_view(template_name="pages/rules.html"), name='rules'),
     path('blog/', include('blog.urls', namespace='blog')),
 ]
