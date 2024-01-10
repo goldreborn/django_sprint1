1e0f2e866243c6b17052e07507f076bb65d8a376
Яков Кузнецов <yakov.kuznetsov98@yandex.ru>
2024-01-10 15:36:20 +0300

    Update

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,5 +1,4 @@
-from django.urls import path, include
-
+from django.urls import path
 from django.views.generic.base import TemplateView
 
 app_name = 'pages'
@@ -8,5 +7,6 @@ urlpatterns = [
     path('about/',
          TemplateView.as_view(template_name="pages/about.html"), name='about'),
     path('rules/',
-         TemplateView.as_view(template_name="pages/rules.html"), name='rules')
+         TemplateView.as_view(template_name="pages/rules.html"), name='rules'),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 """В чем ошибка? рендер используется в возврате функции"""
 
+
 def about(request):
     template = 'pages/about.html'
     return render(request, template)
